8b34cbde058aa6c765de9330d04e57598e92c981
S.watana <75829584+opepelab@users.noreply.github.com>
2021-07-05 02:47:34 +0900

    Update C-x9.c

--- C-x9.c
+++ C-x9.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void){
 
-    int i = 1, b;
+    int i, b;
     scanf("%d",&b);
     
     for (i = 1; i <= 9; i++) {
         printf("%dの数字です\n", i*b);
     }
+    return 0;
 }
