a33ebb6d71aa35db8cd4e9fda9b5f1b21d3a2546
S.watana <75829584+opepelab@users.noreply.github.com>
2021-07-05 02:13:39 +0900

    Add files via upload

---
+++ C-x9.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+
+int main(void){
+
+    int i = 1, b;
+    scanf("%d",&b);
+    
+    for (i = 1; i <= 9; i++) {
+        printf("%dの数字です\n", i*b);
+    return 0;
+        
+    }
+}
